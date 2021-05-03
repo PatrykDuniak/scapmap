3f7d166da19bafb29e11a17f0398d0f0e22c0001
Etherior <znowubezneta@gmail.com>
2021-05-03 15:57:26 +0200

    0.0.3

--- port_scan.py
+++ port_scan.py
@@ -33,16 +33,17 @@ def ping(targets):
     except:
         return -1 
 
-
 def scan_tcp_syn(targets, ports):
     print("Scan TCP SYN on: %s:%s" %(targets, ports))
-    source_port=RandShort()
-    ans = sr1(IP(dst=targets)/TCP(sport=source_port, dport=ports, flags=2),timeout=5, verbose=False)
     
     if ping(targets) != 0:
         return 'Not responding'
     
-    elif ans == None:
+    source_port=RandShort()
+    ans = sr1(IP(dst=targets)/TCP(sport=source_port, dport=ports, flags=2),timeout=5, verbose=False)
+    
+    
+    if ans == None:
         return 'Filtered'
 
     elif ans[TCP].flags == 18:
@@ -60,14 +61,14 @@ def scan_tcp_syn(targets, ports):
 
 def scan_udp(targets, ports):
     print("Scan UDP on: %s:%s" %(targets, ports))
-    source_port=RandShort()
-    ans = sr1(IP(dst=targets)/UDP(sport=source_port, dport=ports),timeout=30, verbose=False)
-    ans.show()
     
     if ping(targets) != 0:
         return 'Not responding'
-
-    elif ans == None:
+    
+    source_port=RandShort()
+    ans = sr1(IP(dst=targets)/UDP(sport=source_port, dport=ports),timeout=30, verbose=False)
+    
+    if ans == None:
         return 'Port open OR filtered'
 
     elif ans.haslayer(UDP):
@@ -85,14 +86,14 @@ def scan_udp(targets, ports):
         
 def scan_tcp_null(targets, ports):
     print("Scan TCP NULL on: %s:%s" %(targets, ports))
+    
+    if ping(targets) != 0:
+        return 'Not responding'
+    
     source_port=RandShort()
     ans = sr1(IP(dst=targets)/TCP(sport=source_port, dport=ports, flags=0),timeout=5, verbose=False)
 
-
-    if ping(targets) != 0:
-        return 'Not responding'
-
-    elif ans == None:
+    if ans == None:
         return 'Port open OR filtered'
         
     elif ans[TCP].flags == 20:
@@ -107,13 +108,15 @@ def scan_tcp_null(targets, ports):
 
 def scan_tcp_fin(targets, ports):
     print("Scan TCP FIN on: %s:%s" %(targets, ports))
+
+    if ping(targets) != 0:
+        return 'Not responding'
+
     source_port=RandShort()
     ans = sr1(IP(dst=targets)/TCP(sport=source_port, dport=ports, flags=1),timeout=5, verbose=False)
     
-    if ping(targets) != 0:
-        return 'Not responding'
 
-    elif ans == None:
+    if ans == None:
         return 'Port open OR filtered'
         
     elif ans[TCP].flags == 20:
@@ -125,15 +128,17 @@ def scan_tcp_fin(targets, ports):
     else:
         return 'Error'
 
+
 def scan_tcp_xmas(targets, ports):
-    print("Scan TCP FIN on: %s:%s" %(targets, ports))
+    print("Scan TCP Xmas on: %s:%s" %(targets, ports))
+    
+    if ping(targets) != 0:
+        return 'Not responding'
+    
     source_port=RandShort()
     ans = sr1(IP(dst=targets)/TCP(sport=source_port, dport=ports, flags=41),timeout=5, verbose=False)
     
-    if ping(targets) != 0:
-        return 'Not responding'
-
-    elif ans == None:
+    if ans == None:
         return 'Port open OR filtered'
         
     elif ans[TCP].flags == 20:
@@ -146,7 +151,76 @@ def scan_tcp_xmas(targets, ports):
         return 'Error'
         
 
-        
+def scan_tcp_ack(targets, ports):
+    print("Scan TCP ACK on: %s:%s" %(targets, ports))
+    
+    if ping(targets) != 0:
+        return 'Not responding'
+
+    source_port=RandShort()
+    ans = sr1(IP(dst=targets)/TCP(sport=source_port, dport=ports, flags=16),timeout=5, verbose=False)
+
+    if ans == None:
+        return 'Filtered'
+
+    elif ans[TCP].flags == 4:
+        return 'Not filtered open/closed'
+
+    elif ans[ICMP].code in [1, 2, 3, 9, 10, 13]:
+        return 'Filtered ICMP unreachable error' 
+
+    else:
+        return 'Error'
+
+
+def scan_tcp_window(targets, ports):
+    print("Scan TCP window on: %s:%s" %(targets, ports))
+    
+    if ping(targets) != 0:
+        return 'Not responding'
+    
+    source_port=RandShort()
+    ans = sr1(IP(dst=targets)/TCP(sport=source_port, dport=ports, flags=16),timeout=5, verbose=False)
+    
+    if ans == None:
+        return 'Filtered'
+
+    elif ans[TCP].flags == 4:
+
+        if ans[TCP].window > 0:
+            return 'Not filtered open'
+
+        elif ans[TCP].window == 0:
+            return 'Not filtered open/closed'
+
+    elif ans[ICMP].code in [1, 2, 3, 9, 10, 13]:
+        return 'Filtered ICMP unreachable error' 
+
+    else:
+        return 'Error'
+    
+   
+def scan_tcp_maimon(targets, ports):
+    print("Scan TCP Xmas on: %s:%s" %(targets, ports))
+    
+    if ping(targets) != 0:
+        return 'Not responding'
+    
+    source_port=RandShort()
+    ans = sr1(IP(dst=targets)/TCP(sport=source_port, dport=ports, flags=17),timeout=5, verbose=False)
+    
+    if ans == None:
+        return 'Port open OR filtered'
+
+    elif ans[TCP].flags == 4:
+        return 'Port closed'
+
+    elif ans[ICMP].code in [1, 2, 3, 9, 10, 13]:
+        return 'Filtered ICMP unreachable error' 
+
+    else:
+        return 'Error'
+
 
 
 if __name__ == "__main__":
@@ -154,7 +228,28 @@ if __name__ == "__main__":
     targets='192.168.1.254'
     ports=80
     #print(scan_tcp_syn(targets, ports))
-    print(scan_udp(targets, ports))
+    #print(scan_udp(targets, ports))
     #print(scan_tcp_null(targets, ports))
     #print(scan_tcp_fin(targets, ports))
     #print(scan_tcp_xmas(targets, ports))
+    #print(scan_tcp_ack(targets,ports))
+    print(scan_tcp_window(targets, ports))
+
+    
+
+
+
+#https://nmap.org/man/pl/man-port-scanning-techniques.html
+
+#Tcp Flags
+#Urgent Pointer 32
+#ACK            16
+#Push            8
+#Reset           4
+#SYN             2
+#FIN             1
+
+#ICMPcode
+#0 - echo reply
+#3 - destination unreachable
+#8 - echo request
