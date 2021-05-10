0eb3febf052bf3dac7abd9c7a9046fb1b224f8fd
Etherior <znowubezneta@gmail.com>
2021-05-10 22:01:53 +0200

    0.0.6

--- port_scan.py
+++ port_scan.py
@@ -1,13 +1,111 @@
 import sys
-import random
 import time   
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.layers.inet import IP, UDP, TCP, ICMP, report_ports, IPTools
-from scapy.sendrecv import sniff, send, sendp, sr1, sr
-from scapy.layers.l2 import Ether
+from scapy.sendrecv import sniff, send, sendp, sr1, sr, srp
+from scapy.layers.l2 import Ether, ARP
 from scapy.compat import raw
 from scapy.volatile import RandShort
+
+class PortScanner():
+    scan_output = {
+        #(1,2,3,9,10,13) : 'Filtered, ICMP unreachable error'
+        'Null' :   { None : 'Port open OR filtered', 20 : 'Port closed [RST(4)+ACK(16)]'},
+        
+        'FIN' :    { None : 'Port open OR filtered', 20 : 'Port closed [RST(4)+ACK(16)]'},
+        
+        'Xmas' :   { None : 'Port open OR filtered', 20 : 'Port closed [RST(4)+ACK(16)]'},
+
+        'SYN':     { None : 'Filtered', 18 : 'Port open, not filtered [SYN(2)+ACK(16)]', 
+                     20 : 'Port closed, propably not filtered [RST(4)+ACK(16)]'},
+
+        'ACK' :    { None : 'Filtered', 4 : 'Not filtered open/closed'},
+        
+        'Window' : { None : 'Filtered', 4 : ['Not filtered open', 'Not filtered open/closed']},
+
+        'Maimon' : { None : 'Port open OR filtered', 4 : 'Port closed'}}
+
+    def __init__(self, targets, ports, type_scan, set_flags):
+        self.__targets = targets
+        self.__ports = ports
+        self.__type_scan = type_scan
+        self.__set_flags = set_flags
+
+    def __tcp_calc_flag(self):
+        flag_value=0
+        flags= {1 : ['FIN', 'F'],
+                2 : ['SYN', 'S'],
+                4 : ['RST', 'R'],
+                8 : ['PSH', 'P'],
+                16: ['ACK', 'A'],
+                32: ['URG', 'U']}
+
+        for word in self.__set_flags.split(' '):
+            for item in flags.items():
+                if(word in item[1]):
+                    flag_value+=item[0]
+
+        return flag_value
+
+    def scan_tcp(self):
+        pick={'SYN' : 2,  'Null' : 0, 'FIN' : 1, 'Xmas' : 41,
+              'ACK' : 16, 'Window' : 16,  'Maimon' : 17}
+        
+        print("Scan TCP %s on: %s:%s" %(self.__type_scan, self.__targets, self.__ports))
+        source_port=RandShort()
+
+        ans = sr1(IP(dst=self.__targets)/TCP(sport=source_port, dport=self.__ports, flags=pick.get(self.__type_scan)),timeout=2, retry=2, verbose=False)
+        
+        try:
+            print(self.scan_output.get(self.__type_scan).get(ans[TCP].flags))
+        except (TypeError, AttributeError):
+            if ans == None:
+                print(self.scan_output.get(self.__type_scan).get(None))
+            else:
+                print('Filtered, ICMP unreachable error')
+                
+    def scan_udp(self):
+        print("Scan UDP on: %s:%s" %(self.__targets, self.__ports))
+        
+        source_port=RandShort()
+        ans = sr1(IP(dst=self.__targets)/UDP(sport=source_port, dport=self.__ports),timeout=2, retry=2, verbose=False)
+        
+        if ans == None:
+            print('Port open OR filtered')
+
+        elif ans.haslayer(UDP):
+            print('Port open, not filtered [Got UDP packet]')
+
+        elif ans[ICMP].code == 3:
+            print('Port closed, not filtered')
+
+        elif ans[ICMP].code in [1,2,9,10,13]:
+            print('Filtered, ICMP unreachable error')
+
+        else:
+            print('Filtered')
+
+    def scan_ip_protocol(self): #https://www.eit.lth.se/ppplab/IPHeader.htm
+        print("Scan IP Protocol on: %s" %(self.__targets))
+
+        for x in range (0, 255):
+            ans = 0
+            ans = sr1(IP(dst=self.__targets, proto=x),timeout=2, retry=2, verbose=False)
+            
+            print("For %d:  " %(x), end = '')
+
+            if ans == None:
+                print('Protocol open OR filtered')
+
+            elif ans[ICMP].code == 2:
+                print('Protocol closed, not filtered')
+
+            elif ans[ICMP].code in [1,3,9,10,13]:
+                print('Filtered, ICMP unreachable error')
+
+            else:
+                print('Protocol open')
 
 
 def ping(targets):
@@ -18,155 +116,16 @@ def ping(targets):
     except:
         return -1 
 
-def tcp_calc_flag(set_flags):
-    flag_value=0
-    flags= { 1 : ['FIN', 'F'],
-             2 : ['SYN', 'S'],
-             4 : ['RST', 'R'],
-             8 : ['PSH', 'P'],
-             16: ['ACK', 'A'],
-             32: ['URG', 'U']}
-
-    for word in set_flags.split(' '):
-        for item in flags.items():
-            if(word in item[1]):
-                flag_value+=item[0]
-
-    return flag_value
-
-def scan_tcp_syn(ans):
-    
-    if ans == None:
-        return 'Filtered'
-
-    elif ans[TCP].flags == 18:
-        return 'Port open, not filtered [SYN(2)+ACK(16)]'
-
-    elif ans[TCP].flags == 20:
-        return 'Port closed, propably not filtered [RST(4)+ACK(16)]'
-
-    elif ans[ICMP].flags in [1,2,3,9,10,13]:
-        return 'Filtered, ICMP unreachable error'
-
-    else:
-        return 'Error'
-        
-def scan_tcp_null_fin_xmas(ans):
-
-    if ans == None:
-        return 'Port open OR filtered'
-        
-    elif ans[TCP].flags == 20:
-        return 'Port closed [RST(4)+ACK(16)]'
-    
-    elif ans[ICMP].code in [1, 2, 3, 9, 10, 13]:
-        return 'Filtered ICMP unreachable error '
-    
-    else:
-        return 'Error'
-    
-def scan_tcp_ack(ans):
-
-    if ans == None:
-        return 'Filtered'
-
-    elif ans[TCP].flags == 4:
-        return 'Not filtered open/closed'
-
-    elif ans[ICMP].code in [1, 2, 3, 9, 10, 13]:
-        return 'Filtered ICMP unreachable error' 
-
-    else:
-        return 'Error'
-
-def scan_tcp_window(ans):
-
-    if ans == None:
-        return 'Filtered'
-
-    elif ans[TCP].flags == 4:
-
-        if ans[TCP].window > 0:
-            return 'Not filtered open'
-
-        elif ans[TCP].window == 0:
-            return 'Not filtered open/closed'
-
-    elif ans[ICMP].code in [1, 2, 3, 9, 10, 13]:
-        return 'Filtered ICMP unreachable error' 
-
-    else:
-        return 'Error'
-    
-def scan_tcp_maimon(ans):
-
-    if ans == None:
-        return 'Port open OR filtered'
-
-    elif ans[TCP].flags == 4:
-        return 'Port closed'
-
-    elif ans[ICMP].code in [1, 2, 3, 9, 10, 13]:
-        return 'Filtered ICMP unreachable error' 
-
-    else:
-        return 'Error'
-
-def scan_tcp(targets, ports, type_scan, set_flags):
-    
-    pick={'SYN'    : [scan_tcp_syn,            2],
-          'Null'   : [scan_tcp_null_fin_xmas,  0],
-          'FIN'    : [scan_tcp_null_fin_xmas,  1],
-          'Xmas'   : [scan_tcp_null_fin_xmas, 41],
-          'ACK'    : [scan_tcp_ack,           16],
-          'Window' : [scan_tcp_window,        16],
-          'Maimon' : [scan_tcp_maimon,        17]}
-    
-    
-    print("Scan TCP %s on: %s:%s" %(type_scan, targets, ports))
-
-    source_port=RandShort()
-    ans = sr1(IP(dst=targets)/TCP(sport=source_port, dport=ports, flags=pick.get(type_scan)[1]),timeout=1, verbose=False)
-    return pick[type_scan][0](ans, targets, ports)
-
-def scan_udp(targets, ports):
-    print("Scan UDP on: %s:%s" %(targets, ports))
-    
-    source_port=RandShort()
-    ans = sr1(IP(dst=targets)/UDP(sport=source_port, dport=ports),timeout=1, verbose=False)
-    
-    if ans == None:
-        return 'Port open OR filtered'
-
-    elif ans.haslayer(UDP):
-        return 'Port open, not filtered [Got UDP packet]'
-
-    elif ans[ICMP].code == 3:
-        return 'Port closed, not filtered'
-
-    elif ans[ICMP].code in [1,2,9,10,13]:
-        return 'Filtered, ICMP unreachable error'
-
-    else:
-        return 'Filtered'
-
-def scan_tcp_protocol(targets, ports): #https://www.eit.lth.se/ppplab/IPHeader.htm
-    print("Scan TCP Protocol on: %s:%s" %(targets, ports))
-    
-    ans = sr1(IP(dst=targets, proto=(0, 255)),timeout=1, verbose=False)
-
-    ans.show()
-    
-
 
 if __name__ == "__main__":
-    random.seed(time.time())
-    targets='192.168.1.254'
+    targets="192.168.1.254"
     ports=80
-    type_scan='FIN'
-    set_flags='A'
-
-    print(scan_tcp_protocol(targets, ports))
+    type_scan="SYN"
+    set_flags="A"
+    port_scanner = PortScanner(targets, ports, type_scan, set_flags)
+    print(port_scanner.scan_tcp())
+    print(port_scanner.scan_udp())
+    print(port_scanner.scan_ip_protocol())
 
 
 #https://nmap.org/man/pl/man-port-scanning-techniques.html
