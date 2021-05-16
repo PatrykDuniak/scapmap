61c57ecaaf6648da53dea97b37d437bde0a98e2a
Etherior <znowubezneta@gmail.com>
2021-05-16 14:59:11 +0200

    0.0.9

--- port_scan.py
+++ port_scan.py
@@ -12,46 +12,42 @@ from scapy.volatile import RandShort
 class IPinterpreter():
     def __init__(self, targets):
         self._targets = targets
-
-    def getTargets(self):
-        return self._targets
+        self.__scope_value=''
 
     def IPcalc(self):
-        scope_value = self._targets.split('.')[-1]
-        if len(scope_value)==1:
+        self.__ipchecker()
+        self.__scope_value = self._targets.split('.')[-1]
+        if len(self.__scope_value)==1:
             return self._targets  
-        elif '-' in scope_value:
+        elif '-' in self.__scope_value:
             self._targets = self._targets.split('.')
-            scope_value = scope_value.split('-')
-            return self._scopeNetwork(scope_value)
-        elif '/' in scope_value:
-            scope_value = scope_value.split('/')
+            self.__scope_value = self.__scope_value.split('-')
+            return self.__scopeNetwork()
+        elif '/' in self.__scope_value:
+            self.__scope_value = self.__scope_value.split('/')
             try:
-                scope_value = list(map(int, scope_value))
+                self.__scope_value = list(map(int, self.__scope_value))
                 self._targets = self._targets.split('.')
             except:
                 print('Wrong Input')
                 return 0
-            return self._subnetCalc(scope_value)
+            return self.__subnetCalc()
 
-    def _scopeNetwork(self, scope_value):
-        self._targets.append('.'.join(self._targets[0:3])+'.'+scope_value[0])
-        self._targets.append('.'.join(self._targets[0:3])+'.'+scope_value[1])
+    def __scopeNetwork(self):
+        self._targets.append('.'.join(self._targets[0:3])+'.'+self.__scope_value[0])
+        self._targets.append('.'.join(self._targets[0:3])+'.'+self.__scope_value[1])
         self._targets=self._targets[-2:]
         return self._targets
             
-    def _subnetCalc(self, scope_value):
+    def __subnetCalc(self):
         subnets=[]
         ip_range=['','']
         self._targets[3]=self._targets[3].split('/')[0]
         hosts=0
-        if scope_value[1] > 30 and scope_value[1] < 1:
-            print('Wrong subnet mask')
-            return 0
 
         for oct in range(1, 5):
-            if(scope_value[1] < 8*oct):
-                hosts=2**(8-(scope_value[1]-8*(oct-1)))
+            if(self.__scope_value[1] < 8*oct):
+                hosts=2**(8-(self.__scope_value[1]-8*(oct-1)))
                 subnets.append(0)
                 for ip in range(256//hosts):
                     subnets.append((hosts*(ip+1)))
@@ -81,6 +77,52 @@ class IPinterpreter():
         self._targets=ip_range
         return self._targets
         
+    def __ipchecker(self):
+        test=self._targets
+        if test.count('.')!=3:
+            print('Not proper IP format')
+            exit()
+        test=test.split('.')
+        for text in range(len(test)-1):
+            try:
+                text=int(text)
+                if text not in range(0,256):
+                    print('Number out of range')
+                    exit()
+            except:
+                print('Not number')
+                exit()
+        try:
+            test[3]=int(test[3])
+        except:
+            if ('-' not in test[3]) and ('/' not in test[3]):
+                print('Only using - and / to setting range')
+                exit()
+            if ('-') in test[3]:
+                test[3]=test[3].split('-')
+                try:
+                    test[3][0] = int(test[3][0])
+                    test[3][1] = int(test[3][1])
+                    if test[3][0] not in range(0, 255) or test[3][1] not in range(0, 255) or test[3][0] > test[3][1]:
+                        print('Last oct number out of range or wrong range')
+                        exit()
+                except:
+                    print('Propably not number')
+                    exit()
+            else:
+                test[3]=test[3].split('/')
+                try:
+                    test[3][0] = int(test[3][0])
+                    test[3][1] = int(test[3][1])
+                    if test[3][0] not in range(0, 255) or test[3][1] not in range(1, 31):
+                        print('Last oct number out of range or wrong subnet number')
+                except:
+                    print('Propably not number')
+                    exit()
+
+
+
+
 
 class PortScanner(IPinterpreter):
     scan_dict = {
@@ -123,14 +165,14 @@ class PortScanner(IPinterpreter):
 
         return flag_value
 
-    def scan_tcp(self, ip):
+    def scan_tcp(self):
         pick={'SYN' : 2,  'Null' : 0, 'FIN' : 1, 'Xmas' : 41,
               'ACK' : 16, 'Window' : 16,  'Maimon' : 17}
         
-        print("Scan TCP %s on: %s:%s" %(self.__type_tcp, ip, self.__ports))
+        print("Scan TCP %s on: %s:%s" %(self.__type_tcp, self._targets, self.__ports))
 
         source_port=RandShort()
-        ans=sr1(IP(dst=ip)/TCP(sport=source_port, dport=self.__ports, flags=pick.get(self.__type_tcp)),timeout=1, retry=1, verbose=False)
+        ans=sr1(IP(dst=self._targets)/TCP(sport=source_port, dport=self.__ports, flags=pick.get(self.__type_tcp)),timeout=1, retry=1, verbose=False)
     
         try:
             if self.__type_scan != 'Window':
@@ -146,11 +188,11 @@ class PortScanner(IPinterpreter):
             else:
                 print('Filtered, ICMP unreachable error')
 
-    def scan_udp(self, ip):
-        print("Scan UDP on: %s:%s" %(ip, self.__ports))
+    def scan_udp(self):
+        print("Scan UDP on: %s:%s" %(self._targets, self.__ports))
         
         source_port=RandShort()
-        ans = sr1(IP(dst=ip)/UDP(sport=source_port, dport=self.__ports),timeout=2, retry=2, verbose=False)
+        ans = sr1(IP(dst=self._targets)/UDP(sport=source_port, dport=self.__ports),timeout=2, retry=2, verbose=False)
         
         if ans == None:
             print('Port open OR filtered')
@@ -167,12 +209,12 @@ class PortScanner(IPinterpreter):
         else:
             print('Filtered')
 
-    def scan_ip_protocol(self, ip): #https://www.eit.lth.se/ppplab/IPHeader.htm
-        print("Scan IP Protocol on: %s" %(ip))
+    def scan_ip_protocol(self): #https://www.eit.lth.se/ppplab/IPHeader.htm
+        print("Scan IP Protocol on: %s" %(self._targets))
 
         for x in range (0, 255):
             ans = 0
-            ans = sr1(IP(dst=ip, proto=x),timeout=2, retry=2, verbose=False)
+            ans = sr1(IP(dst=self._targets, proto=x),timeout=2, retry=2, verbose=False)
             
             print("For %d:  " %(x), end = '')
 
@@ -188,10 +230,10 @@ class PortScanner(IPinterpreter):
             else:
                 print('Protocol open')
 
-    def scan_tcp_custom(self, ip):
+    def scan_tcp_custom(self):
         flag_value=self.__tcp_calc_flag()
         source_port=RandShort()
-        ans=sr1(IP(dst=ip)/TCP(sport=source_port, dport=self.__ports, flags=flag_value),timeout=2, retry=2, verbose=False)
+        ans=sr1(IP(dst=self._targets)/TCP(sport=source_port, dport=self.__ports, flags=flag_value),timeout=2, retry=2, verbose=False)
 
         try:
             ans.show()
@@ -201,49 +243,49 @@ class PortScanner(IPinterpreter):
     def scanner(self):
         base=''
         scan_type = { 'TCP' : self.scan_tcp, 'UDP' : self.scan_udp, 'IP' : self.scan_ip_protocol, 'Custom' : self.scan_tcp_custom}
-        self._targets=self.IPcalc()
-        if type(self._targets) == str:
-            scan_type.get(self.__type_scan)(self._targets)
+        self._targets=range_ip=self.IPcalc()
+        if type(range_ip) == str:
+            scan_type.get(self.__type_scan)()
         else:
-            self._targets[0] = self._targets[0].split('.')
-            self._targets[1] = self._targets[1].split('.')
+            range_ip[0] = range_ip[0].split('.')
+            range_ip[1] = range_ip[1].split('.')
             for oct in range(4):
-                if self._targets[0][oct] == self._targets[1][oct]:
+                if range_ip[0][oct] == range_ip[1][oct]:
                     continue
                 else:
                     if oct == 3:
-                        for ip in range(int(self._targets[0][oct]), int(self._targets[1][oct])+1):
-                            scan_type.get(self.__type_scan)(self._targets[0][0]+'.'+self._targets[0][1]+'.'+self._targets[0][2]+'.'+str(ip))
+                        for ip in range(int(range_ip[0][oct]), int(range_ip[1][oct])+1):
+                            self._targets=range_ip[0][0]+'.'+range_ip[0][1]+'.'+range_ip[0][2]+'.'+str(ip)
+                            scan_type.get(self.__type_scan)()
                     else:
                         for set in range(0, oct):
-                                base+=self._targets[0][set]+'.'
-                        for ip in range(int(self._targets[0][oct]), int(self._targets[1][oct])):
+                                base+=range_ip[0][set]+'.'
+                        for ip in range(int(range_ip[0][oct]), int(range_ip[1][oct])):
                             if base.count('.')==0:
                                 for x in range(1, 255):
                                     for y in range(1, 255):
                                         for z in range(1, 255):
-                                            scan_type.get(self.__type_scan)(str(ip)+'.'+str(x)+'.'+str(y)+'.'+str(z))
+                                            self._targets=str(ip)+'.'+str(x)+'.'+str(y)+'.'+str(z)
+                                            scan_type.get(self.__type_scan)()
                             elif base.count('.')==1:
                                 for x in range(1, 255):
                                     for y in range(1, 255):
-                                        scan_type.get(self.__type_scan)(base+str(ip)+'.'+str(x)+'.'+str(y))
+                                        self._targets=base+str(ip)+'.'+str(x)+'.'+str(y)
+                                        scan_type.get(self.__type_scan)
                             else:
                                 for x in range(1, 255):
-                                    scan_type.get(self.__type_scan)(base+str(ip)+'.'+str(x))
+                                    self._targets=base+str(ip)+'.'+str(x)
+                                    scan_type.get(self.__type_scan)()
 
    
 if __name__ == "__main__":
-    targets="192.168.1.42/24"
+    targets="192.168.1.1/32"
     ports=80
     type_scan="TCP"
     set_flags="A"
 
-
     port_scanner = PortScanner(targets, ports, type_scan)
     port_scanner.scanner()
-
-    ipchange=IPinterpreter(targets)
-    print(ipchange.IPcalc())
 
 
 
