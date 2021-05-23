df0494034a7e43defc908df2c58135d00fcdfa06
Etherior <znowubezneta@gmail.com>
2021-05-23 20:39:17 +0200

    0.1.3

--- PortScanner.py
+++ PortScanner.py
@@ -1,123 +1,9 @@
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-from scapy.layers.inet import IP, UDP, TCP, ICMP, report_ports, IPTools
-from scapy.sendrecv import sniff, send, sendp, sr1, sr, srp
-from scapy.layers.l2 import Ether, ARP
-from scapy.compat import raw
+from scapy.layers.inet import IP, UDP, TCP, ICMP
+from scapy.sendrecv import sr1
 from scapy.volatile import RandShort
-
-
-class IPinterpreter():
-    def __init__(self, targets):
-        self._targets = targets
-        self.__scope_value=''
-
-    def IPcalc(self):
-        self.__ipchecker()
-        self.__scope_value = self._targets.split('.')[-1]
-
-        if '-' in self.__scope_value:
-            self._targets = self._targets.split('.')
-            self.__scope_value = self.__scope_value.split('-')
-            return self.__scopeNetwork()
-        elif '/' in self.__scope_value:
-            self.__scope_value = self.__scope_value.split('/')
-            try:
-                self.__scope_value = list(map(int, self.__scope_value))
-                self._targets = self._targets.split('.')
-            except:
-                print('Wrong Input')
-                return 0
-            return self.__subnetCalc()
-        else:
-            return self._targets
-
-    def __scopeNetwork(self):
-        self._targets.append('.'.join(self._targets[0:3])+'.'+self.__scope_value[0])
-        self._targets.append('.'.join(self._targets[0:3])+'.'+self.__scope_value[1])
-        self._targets=self._targets[-2:]
-        return self._targets
-
-    def __subnetCalc(self):
-        subnets=[]
-        ip_range=['','']
-        self._targets[3]=self._targets[3].split('/')[0]
-        hosts=0
-
-        for oct in range(1, 5):
-            if(self.__scope_value[1] < 8*oct):
-                hosts=2**(8-(self.__scope_value[1]-8*(oct-1)))
-                subnets.append(0)
-                for ip in range(256//hosts):
-                    subnets.append((hosts*(ip+1)))
-                for inx in range(len(subnets)):
-                    if int(self._targets[oct-1]) in range(subnets[inx], subnets[inx+1]):
-                        for step in range(0,4):
-                            if (oct-1)==step:
-                                ip_range[0]+=str(subnets[inx]+1)+'.'
-                                ip_range[1]+=str(subnets[inx+1]-1)+'.'
-                                break
-                            else:
-                                ip_range[0]+=self._targets[step]+'.'
-                                ip_range[1]+=self._targets[step]+'.'
-                        break
-                
-                for _ in range(4-ip_range[0].count('.')):
-                    ip_range[0]+='1.'
-                    if(ip_range[1].count('.')==3):
-                        ip_range[1]+='254.'
-                    else:
-                        ip_range[1]+='255.'
-
-                ip_range[0]=ip_range[0][:-1]
-                ip_range[1]=ip_range[1][:-1]
-                break
-
-        self._targets=ip_range
-        return self._targets
-
-    def __ipchecker(self):
-        test=self._targets
-        if test.count('.')!=3:
-            print('Not proper IP format')
-            exit()
-        test=test.split('.')
-        for text in range(len(test)-1):
-            try:
-                text=int(text)
-                if text not in range(0,256):
-                    print('Number out of range')
-                    exit()
-            except:
-                print('Not number')
-                exit()
-        try:
-            test[3]=int(test[3])
-        except:
-            if ('-' not in test[3]) and ('/' not in test[3]):
-                print('Only using - and / to setting range')
-                exit()
-            if ('-') in test[3]:
-                test[3]=test[3].split('-')
-                try:
-                    test[3][0] = int(test[3][0])
-                    test[3][1] = int(test[3][1])
-                    if test[3][0] not in range(0, 255) or test[3][1] not in range(0, 255) or test[3][0] > test[3][1]:
-                        print('Last oct number out of range or wrong range')
-                        exit()
-                except:
-                    print('Propably not number')
-                    exit()
-            else:
-                test[3]=test[3].split('/')
-                try:
-                    test[3][0] = int(test[3][0])
-                    test[3][1] = int(test[3][1])
-                    if test[3][0] not in range(0, 255) or test[3][1] not in range(1, 31):
-                        print('Last oct number out of range or wrong subnet number')
-                except:
-                    print('Propably not number')
-                    exit()
+from IPinterpreter import IPinterpreter
 
 
 class PortScanner(IPinterpreter):
@@ -347,23 +233,9 @@ class PortScanner(IPinterpreter):
                                     self._targets=base+str(ip)+'.'+str(x)
                                     self.__portFor()
 
-  
-if __name__ == "__main__":
-    targets="192.168.1.254"
-    ports='0-65534'
-    type_scan="TCP"
-    set_flags="A"
-    only_open=True
-
-    port_scanner = PortScanner(targets, ports, type_scan)
-    port_scanner.scanner()
-    input()
 
 
 
-
-
-#https://nmap.org/man/pl/man-port-scanning-techniques.html
 
 #Tcp Flags
 #URG(Urgent)            32
